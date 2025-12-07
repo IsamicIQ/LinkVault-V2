dbbbaacfab2e7dcdd1cb782147d74ac0db125628
Yasirgit <ysrmoha098.com>
2025-12-07 14:02:42 +0300

    Exclude src-vite from Next.js TypeScript compilation to fix build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude src-vite directory from Next.js build
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
